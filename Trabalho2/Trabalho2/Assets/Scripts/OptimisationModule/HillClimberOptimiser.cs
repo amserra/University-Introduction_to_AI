d69e56ab30112a6d760a4b15d87a1e513df2bcf6
joaopdinis <joaopedro1099@hotmail.com>
2020-03-26 11:16:21 +0000

    Criacao Projeto Trabalho2

---
+++ Trabalho2/Trabalho2/Assets/Scripts/OptimisationModule/HillClimberOptimiser.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.IO;
+
+
+public class HillClimberOptimiser : OptimisationAlgorithm
+{
+
+    private int bestCost;
+    private List<int> newSolution = null;
+    
+
+    string fileName = "Assets/Logs/" + System.DateTime.Now.ToString("ddhmmsstt") + "_HillClimberOptimiser.csv";
+
+
+    protected override void Begin()
+    {
+        CreateFile(fileName);
+        // Initialization
+        /* *****************************************
+                        YOUR CODE HERE
+            *****************************************                      
+        */
+    }
+
+    protected override void Step()
+    {
+    
+        /* *****************************************
+                        YOUR CODE HERE
+            *****************************************                      
+        */
+
+        //DO NOT CHANGE THE LINES BELLOW
+        AddInfoToFile(fileName, base.CurrentNumberOfIterations, this.Evaluate(base.CurrentSolution), base.CurrentSolution);
+        base.CurrentNumberOfIterations++;
+
+    }
+
+   
+
+}
---
+++ Trabalho2/Trabalho2/Assets/Scripts/OptimisationModule/SimulatedAnnealingOptimiser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulatedAnnealingOptimiser : OptimisationAlgorithm
+{
+    private List<int> newSolution = null;
+    private int CurrentSolutionCost;
+    public float Temperature;
+    private float zero = Mathf.Pow(10, -6);// numbers bellow this value can be considered zero.
+
+    string fileName = "Assets/Logs/" + System.DateTime.Now.ToString("ddhmmsstt") + "_SimulatedAnnealingOptimiser.csv";
+
+
+    protected override void Begin()
+    {
+        CreateFileSA(fileName);
+        // Initialization
+        /* *****************************************
+                        YOUR CODE HERE
+            *****************************************                      
+        */
+    }
+
+    protected override void Step()
+    {
+
+        /* *****************************************
+                        YOUR CODE HERE
+            *****************************************                      
+        */
+
+        //DO NOT CHANGE THE LINES BELLOW
+        AddInfoToFile(fileName, base.CurrentNumberOfIterations, CurrentSolutionCost, CurrentSolution, Temperature);
+        base.CurrentNumberOfIterations++;
+
+    }
+
+
+}
